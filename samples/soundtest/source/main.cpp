e1b01aee7dc6f8aeca1d1fdcd58c9bda8a2aad87
Dan Gerendasy <werewu45@yahoo.com>
2016-11-18 15:02:49 -0800

    Added scetool flags to soundtest sample
    Reduced stack footprint in soundtest sample

--- samples/soundtest/source/main.cpp
+++ samples/soundtest/source/main.cpp
@@ -14,18 +14,21 @@ int drawUpdate(float deltaTime, unsigned int frame);
 void padUpdate(int changed, int port, padData pData);
 void exit();
 
-Mini2D mini((Mini2D::PadCallback_f)&padUpdate, (Mini2D::DrawCallback_f)&drawUpdate, (Mini2D::ExitCallback_f)&exit);
+Mini2D * mini = NULL;
+
+Font * font1 = NULL;
+Sound * sound1 = NULL;
+Sound * sound2 = NULL;
 
 int doExit = 0;
-Font *font1;
-Sound *sound1, *sound2;
 int infiniteVoice = -1;
 int status;
 
-const Vector2 FONT_MEDIUM(0.04*mini.MAXW, 0.04*mini.MAXH);
-
-const Vector2 PRINT_ITEM0(0.5*mini.MAXW, 0.5*mini.MAXH);
-const Vector2 PRINT_ITEM1(PRINT_ITEM0.X, 0.55*mini.MAXH);
+// Font sizes
+Vector2 FONT_MEDIUM;
+// Font locations
+Vector2 PRINT_ITEM0;
+Vector2 PRINT_ITEM1;
 
 std::wstring TEXT_PAUSEAUDIO = 			L"Press Triangle to pause audio.";
 std::wstring TEXT_RESUMEAUDIO = 		L"Press Triangle to resume audio.";
@@ -33,23 +36,35 @@ std::wstring TEXT_STARTAUDIO = 			L"Press Triangle to start infinite audio.";
 std::wstring TEXT_SNARE =				L"Press Square for snare.";
 
 int main(s32 argc, const char* argv[]) {
-	font1 = new Font(&mini);
+
+	// Load Mini2D
+	mini = new Mini2D((Mini2D::PadCallback_f)&padUpdate, (Mini2D::DrawCallback_f)&drawUpdate, (Mini2D::ExitCallback_f)&exit);
+
+	// Initialize location and size vectors
+	FONT_MEDIUM = Vector2(0.04*mini->MAXW, 0.04*mini->MAXH);
+
+	PRINT_ITEM0 = Vector2(0.5*mini->MAXW, 0.5*mini->MAXH);
+	PRINT_ITEM1 = Vector2(PRINT_ITEM0.X, 0.55*mini->MAXH);
+
+	// Load comfortaa font
+	font1 = new Font(mini);
 	font1->TextAlign = Font::PRINT_ALIGN_CENTER;
 	if (font1->Load((void*)comfortaa_regular_ttf, comfortaa_regular_ttf_size))
 		printf("error loading font\n");
 
-	sound1 = new Sound(&mini);
-	sound2 = new Sound(&mini);
+	// Initialize sounds
+	sound1 = new Sound(mini);
+	sound2 = new Sound(mini);
 
 	// Load adhdj song, 101 seconds
 	sound1->Load(sbfsc_adhdj_mp3, sbfsc_adhdj_mp3_size, 101);
 	// Load snare sound, 0.4 seconds
 	sound2->Load(snare_ogg, snare_ogg_size, 0.4f);
 
-	mini.SetAnalogDeadzone(15);
-	mini.SetClearColor(0x23B2D7FF);
-	mini.SetAlphaState(1);
-	mini.BeginDrawLoop();
+	mini->SetAnalogDeadzone(15);
+	mini->SetClearColor(0x23B2D7FF);
+	mini->SetAlphaState(1);
+	mini->BeginDrawLoop();
 
 	return 0;
 }
@@ -99,4 +114,24 @@ void padUpdate(int changed, int port, padData pData) {
 
 void exit() {
 	printf("exiting\n");
+
+	if (sound1) {
+		delete sound1;
+		sound1 = NULL;
+	}
+
+	if (sound2) {
+		delete sound2;
+		sound2 = NULL;
+	}
+
+	if (font1) {
+		delete font1;
+		font1 = NULL;
+	}
+
+	if (mini) {
+		delete mini;
+		mini = NULL;
+	}
 }
