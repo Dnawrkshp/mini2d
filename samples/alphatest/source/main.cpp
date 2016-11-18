4c8075fb979d065db7b727998dc685d0852d50e1
Dan Gerendasy <werewu45@yahoo.com>
2016-11-18 15:24:27 -0800

    Added scetool flags to alphatest sample
    Reduced stack footprint in alphatest sample

--- samples/alphatest/source/main.cpp
+++ samples/alphatest/source/main.cpp
@@ -10,25 +10,34 @@ int drawUpdate(float deltaTime, unsigned int frame);
 void padUpdate(int changed, int port, padData pData);
 void exit();
 
+Mini2D * mini = NULL;
+Image * img1 = NULL;
+
 int doExit = 0;
 int doSaturate = 0;
-Image *img1;
 
-Mini2D mini((Mini2D::PadCallback_f)&padUpdate, (Mini2D::DrawCallback_f)&drawUpdate, (Mini2D::ExitCallback_f)&exit);
-
-Vector2 ALPHATEST_LOC(0.5*mini.MAXW,0.5*mini.MAXH);
-Vector2 ALPHATEST_SIZE(0.5*mini.MAXW,0.5*mini.MAXH);
+Vector2 ALPHATEST_LOC;
+Vector2 ALPHATEST_SIZE;
 
 int main(s32 argc, const char* argv[]) {
-	img1 = new Image(&mini);
+
+	// Initialize Mini2D
+	mini = new Mini2D((Mini2D::PadCallback_f)&padUpdate, (Mini2D::DrawCallback_f)&drawUpdate, (Mini2D::ExitCallback_f)&exit);
+
+	// Initialize location and size vectors
+	ALPHATEST_LOC = Vector2(0.5*mini->MAXW,0.5*mini->MAXH);
+	ALPHATEST_SIZE = Vector2(0.5*mini->MAXW,0.5*mini->MAXH);
+
+	// Load image
+	img1 = new Image(mini);
 	img1->Load((void*)alphatest_png, alphatest_png_size, Image::IMAGE_TYPE_PNG);
 	img1->DrawRegion.Location = ALPHATEST_LOC;
 	img1->DrawRegion.Dimension = ALPHATEST_SIZE;
 
-	mini.SetAnalogDeadzone(15);
-	mini.SetClearColor(0x23B2D7FF);
-	mini.SetAlphaState(1);
-	mini.BeginDrawLoop();
+	mini->SetAnalogDeadzone(15);
+	mini->SetClearColor(0x23B2D7FF);
+	mini->SetAlphaState(1);
+	mini->BeginDrawLoop();
 
 	return 0;
 }
@@ -50,4 +59,14 @@ void padUpdate(int changed, int port, padData pData) {
 
 void exit() {
 	printf("exiting\n");
+
+	if (img1) {
+		delete img1;
+		img1 = NULL;
+	}
+
+	if (mini) {
+		delete mini;
+		mini = NULL;
+	}
 }
